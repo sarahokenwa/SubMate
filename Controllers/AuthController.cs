2e95cd358b423240bd1daf9a0c32dc40b6ce6dd1
Sarah Okenwa <sarahokenwa@gmail.com>
2025-08-31 23:52:02 +0100

    Add Authentication and User Service

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SubMate.Core.Dtos.Auth;
+using SubMate.Core.Interfaces.Services;
+
+namespace SubMate.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(IAuthService authService, ILogger<AuthController> logger)
+        {
+            _authService = authService;
+            _logger = logger;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request, CancellationToken ct)
+        {
+
+            var response = await _authService.RegisterAsync(request, ct);
+            return Ok(response);
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request, CancellationToken ct)
+        {
+
+            var response = await _authService.LoginAsync(request, ct);
+            return Ok(response);
+        }
+    }
+}
+
---
+++ Extensions/ServicesExtensions.cs
@@ -0,0 +1,41 @@
+﻿using SubMate.Core.Interfaces.Repositories;
+using SubMate.Core.Interfaces.Services;
+using SubMate.Infrastructure.Repositories;
+using SubMate.Infrastructure.Services;
+
+namespace SubMate.Api.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void AddAppServices(this IServiceCollection services,
+           IConfiguration configuration)
+        {
+            //Services
+            //services.AddScoped<IScheduleManager, ScheduleManager>();
+            
+
+
+
+            //Repositories
+            #region Repositories
+            services.AddScoped<IUserRepository, UserRepository>();
+            
+
+            #endregion
+
+            //Third party service
+            #region service
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IUserService, UserService>();
+           
+
+
+            #endregion service
+
+            //Configurations
+            //services.AddSingleton(configuration.GetSection("ApiOptions").Get<ApiOptions>());
+            
+
+        }
+    }
+}
